7f369607655b38910fca31f8d84c84149db90207
Bernard D. White <danquahwhite@gmail.com>
2019-04-21 04:35:16 +0000

    created an overload vote method that uses lang control structures such as struct and mapping

--- contracts/Voter.sol
+++ contracts/Voter.sol
@@ -4,13 +4,27 @@ pragma experimental ABIEncoderV2;
 
 contract Voter {
   
+  struct Candidate{
+    uint position;
+    bool exist;
+  }
+
   uint[] public votes;
   string[] public candidates;
   mapping (address => bool) hasVoted;
+  mapping (string => Candidate) posOfCandidate;
+
+  
   
   constructor(string[] memory _candidates) public {
     candidates = _candidates;
     votes.length = candidates.length;
+
+    for(uint i = 0; i < candidates.length; i++ ){
+      Candidate memory candidate =  Candidate(i,true);
+      string memory candidateName =candidates[i];
+      posOfCandidate[candidateName] = candidate;
+    }
   }
 
   //use to cast a vote. Acceppts the index of candidate in candidates[]
@@ -20,7 +34,21 @@ contract Voter {
     require(0 <= candidate && candidate < candidates.length, "Invalid candidate");
     require(!hasVoted[msg.sender], "Account has already voted");
 
+
     votes[candidate] = votes[candidate] + 1;
+    hasVoted[msg.sender] = true;
+  }
+
+  function vote(string memory candidateName) public {
+    require(!hasVoted[msg.sender], "Account has already voted");
+
+    Candidate memory candidate = posOfCandidate[candidateName];
+
+    if((candidate.position == 0 && !candidate.exist)){
+        revert("Candidate does not exist");
+    }
+    
+    votes[candidate.position] = votes[candidate.position] +1;
     hasVoted[msg.sender] = true;
   }
 
